8b10b5f68731ab0b50c481653f3e62d6757ff1d3
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-05 18:46:48 -0700

    allow for place entry

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # resources :users, only:[:show]
   root "home#index"
 
-
+  resources :users, only: [:index]
   namespace :user do
     get "users/:id", to: "dashboard#show", as: :dashboard
     get 'fuel_stations/:id', to: "dashboard#fuel_stations", as: :fuel_stations
