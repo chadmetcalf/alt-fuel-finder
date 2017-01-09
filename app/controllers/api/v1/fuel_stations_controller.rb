ecd379afa50cbdabf74573348be04252274af683
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-08 17:10:20 -0700

    get desired response in the controller, not test

--- app/controllers/api/v1/fuel_stations_controller.rb
+++ app/controllers/api/v1/fuel_stations_controller.rb
@@ -2,9 +2,10 @@ class Api::V1::FuelStationsController < ApplicationController
   def index
     lat = params[:lat]
     lng = params[:lng]
-
-    Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_API']}&latitude=#{lat}&longitude=#{lng}&radius=15.0")
+    fuel = params[:fuel_type]
+    response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_KEY']}&latitude=#{lat}&longitude=#{lng}&fuel_type=#{fuel}&radius=15.0")
     parsed = JSON.parse(response.body, symbolize_names: true)
+    parsed[:fuel_stations]
     binding.pry
   end
 end
--- spec/requests/api/v1/fuel_stations_request_spec.rb
+++ spec/requests/api/v1/fuel_stations_request_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 describe "fuel stations endpoints" do
   context "GET /fuel_stations" do
     it "returns fuel stations for the specified query params" do
-      get "/api/v1/fuel_stations?lat=39.7392&lng=-104.9903"
+
+      get "/api/v1/fuel_stations?lat=39.7392&lng=-104.9903&fuel_type=E85"
+
+      result = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(response).to be(Array)
+      expect(response.count).to eq(14)
     end
   end
 end
