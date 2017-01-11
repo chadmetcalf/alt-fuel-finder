d1f40eb5c1fc78d2d112d1241adfe294cc6524da
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-10 17:31:58 -0700

    pass internal api test

--- app/controllers/api/v1/fuel_stations_controller.rb
+++ app/controllers/api/v1/fuel_stations_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::FuelStationsController < ApplicationController
   def index
-    parameters = params.select { |k,v| k.include?("coords") }
+    parameters = params.select { |k,v| k.include?("latLng") }
     fuel = params["fuel_type"]
-    fuel_stations = parameters.map do |k, v|
+    stations = []
+    parameters.each do |k, v|
       lat = v.split(",")[0]
       lng = v.split(",")[1]
       response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_KEY']}&latitude=#{lat}&longitude=#{lng}&fuel_type=#{fuel}&radius=30.0&limit=1")
       parsed = JSON.parse(response.body, symbolize_names: true)
-      parsed_data = parsed[:fuel_stations]
+      stations << parsed[:fuel_stations].first
     end
-    render json: fuel_stations
+    render json: stations
   end
 end
--- spec/requests/api/v1/fuel_stations_request_spec.rb
+++ spec/requests/api/v1/fuel_stations_request_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 describe "fuel stations endpoints" do
   context "GET /fuel_stations" do
-    it "returns electric fuel stations for 200 mile intervals between Denver and Boston" do
+    it "returns electric fuel station for every 200 mile interval between Denver and Boston" do
 
-      params = {"coords0"=>"41.11496,-101.71232", "coords1"=>"40.821110000000004,-97.63129", "coords2"=>"41.541380000000004,-94.00112000000001", "coords3"=>"41.43824,-90.17155000000001", "coords4"=>"41.72204000000001,-86.24844", "coords5"=>"41.334680000000006,-82.3807", "coords6"=>"42.65402,-78.96629", "coords7"=>"43.13613,-75.26988", "coords8"=>"42.191500000000005,-71.85867", "fuel_type"=>"ELEC"}
+      params = {
+        "latLng0"=>"41.11496,-101.71232", "latLng1"=>"40.821110000000004,-97.63129",
+        "latLng2"=>"41.541380000000004,-94.00112000000001",
+        "latLng3"=>"41.43824,-90.17155000000001",
+        "latLng4"=>"41.72204000000001,-86.24844", "latLng5"=>"41.334680000000006,-82.3807",
+        "latLng6"=>"42.65402,-78.96629", "latLng7"=>"43.13613,-75.26988",
+        "latLng8"=>"42.191500000000005,-71.85867", "fuel_type"=>"ELEC"
+      }
 
       get "/api/v1/fuel_stations?", params
 
@@ -13,17 +20,18 @@ describe "fuel stations endpoints" do
       expect(response).to be_success
       expect(result.class).to eq(Array)
       expect(result.count).to eq(9)
-      binding.pry
+
+      expect(result.first[:id]).to eq(51672)
       expect(result.first[:station_name]).to eq("OGALLALA")
-      expect(result.first[:latitude]).to eq(41.1257932)
-      expect(result.first[:longitude]).to eq(41.1257932)
       expect(result.first[:access_days_time]).to eq("24 hours daily")
+      expect(result.first[:latitude]).to eq(41.1257932)
+      expect(result.first[:longitude]).to eq(-101.715413)
 
-
+      expect(result.last[:id]).to eq(45875)
       expect(result.last[:station_name]).to eq("Bertera Nissan")
+      expect(result.last[:access_days_time]).to eq(nil)
       expect(result.last[:latitude]).to eq(42.1828905)
       expect(result.last[:longitude]).to eq(-71.8513063)
-      expect(result.last[:access_days_time]).to eq(nil)
     end
   end
 end
